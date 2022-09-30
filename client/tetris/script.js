d4ccd35da62bf4050db838103ee9fa91b86ae19a
EZ-19 <111812260+EZ-19@users.noreply.github.com>
2022-09-30 19:43:49 -0400

    add initial css, html, and js files

---
+++ client/tetris/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const GRID = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const SCORE_DISPLAY = document.querySelector('#score');
+  const START_BTN = document.querySelector('#start-button');
+  const WIDTH = 10;
+
+  // The Tetriminoes
+  const lTetromino = [
+    [1, WIDTH + 1, WIDTH * 2 + 1, 2],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 2],
+    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 2],
+    [WIDTH, WIDTH * 2, WIDTH * 2 + 1, WIDTH * 2 + 2]
+  ];
+
+  const zTetromino = [
+    [0, WIDTH, WIDTH + 1, WIDTH * 2 + 1],
+    [WIDTH + 1, WIDTH + 2, WIDTH * 2, WIDTH * 2 + 1],
+    [0, WIDTH, WIDTH + 1, WIDTH * 2 + 1],
+    [WIDTH + 1, WIDTH + 2, WIDTH * 2, WIDTH * 2 + 1]
+  ];
+
+  const tTetromino = [
+    [1, WIDTH, WIDTH + 1, WIDTH + 2],
+    [1, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 1],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH * 2 + 1],
+    [1, WIDTH, WIDTH + 1, WIDTH * 2 + 1]
+  ];
+
+  const oTetromino = [
+    [0, 1, WIDTH, WIDTH + 1],
+    [0, 1, WIDTH, WIDTH + 1],
+    [0, 1, WIDTH, WIDTH + 1],
+    [0, 1, WIDTH, WIDTH + 1]
+  ];
+
+  const iTetromino = [
+    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 3 + 1],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH + 3],
+    [1, WIDTH + 1, WIDTH * 2 + 1, WIDTH * 3 + 1],
+    [WIDTH, WIDTH + 1, WIDTH + 2, WIDTH + 3]
+  ];
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+
+  let currentPosition = 4;
+  let current = theTetrominoes[0][0];
+
+  
+});
