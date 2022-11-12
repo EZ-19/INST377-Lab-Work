e0b8c0685cea6d3f90c01992d6c002b8e7f71499
EZ-19 <111812260+EZ-19@users.noreply.github.com>
2022-11-12 18:37:21 -0500

    Added Leaflet Setup

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -83,6 +83,16 @@ function filterList(array, filterInputValue) {
   return newArray;
 }
 
+function initMap() {
+  console.log('init map');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
 async function mainEvent() {
   /*
       ## Main Event
@@ -90,6 +100,8 @@ async function mainEvent() {
         When you're not working in a heavily-commented "learning" file, this also is more legible
         If you separate your work, when one piece is complete, you can save it and trust it
     */
+
+  const pageMap = initMap();
 
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
